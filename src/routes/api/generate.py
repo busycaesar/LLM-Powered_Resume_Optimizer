341392c9934ef2d17fae8e0df8def8cccaa2d2db
Dev <busycaesar@gmail.com>
2025-03-03 11:04:26 -0500

    Add(api/generate): pseudo code for API function

--- src/routes/api/generate.py
+++ src/routes/api/generate.py
@@ -5,6 +5,36 @@ generate_api = Blueprint("generate_api", __name__)
 
 @generate_api.route("/skills", methods=["POST"])
 def post_skills():
-    # Takes the document link from the request body.
-    # Stores the document link in the database and sends a positive response.
-    return jsonify(response(True, "Resume document link is stored.")), 200
+    # Takes the stored job description.
+    # Gets the content of the resume using the stored document link.
+    # Adds the job description and resume content into the prompt template.
+    # Sends the prompt template to the LLM to generate the response.
+    # Returns a positive response along with the generated response.
+    return jsonify(response(True, "Suggested skills are sent.")), 200
+
+@generate_api.route("/projects", methods=["POST"])
+def post_projects():
+    # Takes the stored job description.
+    # Gets the relevant master data content chunk from the vector database.
+    # Adds the job description and relevant content chunk into the prompt template.
+    # Sends the prompt template to the LLM to generate the response.
+    # Returns a positive response along with the generated response.
+    return jsonify(response(True, "Suggested projects along with updated description are sent.")), 200
+
+@generate_api.route("/summary", methods=["POST"])
+def post_summary():
+    # Takes the stored job description.
+    # Gets the content of the resume using the stored document link.
+    # Adds the job description and resume content into the prompt template.
+    # Sends the prompt template to the LLM to generate the response.
+    # Returns a positive response along with the generated response.
+    return jsonify(response(True, "Suggested summary is sent.")), 200
+
+@generate_api.route("/ats-check", methods=["POST"])
+def post_ats_check():
+    # Takes the stored job description.
+    # Gets the content of the resume using the stored document link.
+    # Adds the job description and resume content into the prompt template.
+    # Sends the prompt template to the LLM to generate the response.
+    # Returns a positive response along with the generated response.
+    return jsonify(response(True, "Resume's ATS score and suggests the updates for increasing the score is sent.")), 200
